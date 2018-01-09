f1096b22d1f72e767f851ec482e70ab567bf9e30
Andela Developer <andeladeveloper@Andelas-MacBook-Pro-6.local>
2018-01-09 19:23:32 +0100

    feat(search): implement real time search - should display real time result as user types at least three (3) characters - - should display no product(s) found if the searchTerm does not match any product [Finishes #153740730]

--- imports/plugins/included/search-mongo/server/publications/searchresults.js
+++ imports/plugins/included/search-mongo/server/publications/searchresults.js
@@ -11,8 +11,13 @@ const supportedCollections = ["products", "orders", "accounts"];
 function getProductFindTerm(searchTerm, searchTags, userId) {
   const shopId = Reaction.getShopId();
   const findTerm = {
-    shopId: shopId,
-    $text: { $search: searchTerm }
+    $and: [
+      { shopId: shopId },
+      { title: {
+        $regex: searchTerm,
+        $options: "i"
+      } }
+    ]
   };
   if (searchTags.length) {
     findTerm.hashtags = { $all: searchTags };
@@ -148,7 +153,7 @@ Meteor.publish("SearchResults", function (collection, searchTerm, facets, maxRes
   check(searchTerm, Match.Optional(String));
   check(facets, Match.OneOf(Array, undefined));
   Logger.debug(`Returning search results on ${collection}. SearchTerm: |${searchTerm}|. Facets: |${facets}|.`);
-  if (!searchTerm) {
+  if (!searchTerm || (collection === "products" && searchTerm.length < 3 )) {
     return this.ready();
   }
   return getResults[collection](searchTerm, facets, maxResults, this.userId);
